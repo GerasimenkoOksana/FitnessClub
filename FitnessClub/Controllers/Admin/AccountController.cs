6b47ef76c3250cdf3e5c3269cec4b0bcf02f4448
Oksana Gerasimenko <gerrus155@gmail.com>
2021-09-09 00:38:10 +0300

    2 - начало настройки админ и аккаунт

---
+++ FitnessClub/Controllers/Admin/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using FitnessClub.Data.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitnessClub.Controllers.Admin
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ProjectUser> _userManager;
+        private readonly SignInManager<ProjectUser> _signInManager;
+        public AccountController(UserManager<ProjectUser> userManager, SignInManager<ProjectUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        public IActionResult Index()
+        {
+            return View("~/Views/Admin/Account/Index.cshtml", _userManager.Users);
+        }
+    }
+}
